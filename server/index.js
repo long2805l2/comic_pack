53db74de16f6ada971f3a60d5fef232513cabacf
skyer <long2805l2@gmail.com>
2018-07-05 14:51:52 +0700

    +add upload file -move code index.html to gallery.html

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ var API = require ('./js/api.js');
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
-app.use(bodyParser.raw());
+// app.use(bodyParser.raw());
 
 app.use(express.static(__dirname + '/node_modules'));
 app.use('/assets', express.static(__dirname + '/client/assets'));
@@ -19,10 +19,36 @@ app.use('/js/protobuf.js', express.static(__dirname + '/node_modules/protobufjs/
 
 app.get('/', function(request, response, next)
 {
+	response.status (200);
 	response.sendFile(__dirname + "/client/index.html");
 });
 
-app.post('/request', function(request, response, next)
+app.get('/favicon.ico', function(request, response, next)
+{
+	response.status (200);
+	response.sendFile(__dirname + "/client/assets/ico/favicon.ico");
+});
+
+app.get('/:page', function(request, response, next)
+{
+	let page = request.params.page;
+	let path = __dirname + "/client/" + page;
+	if (page.endsWith (".html") && fs.existsSync (path))
+	{
+		response.status (200);
+		response.sendFile(path);
+		return;
+	}
+
+	response.status (404);
+	response.sendFile("Page Not Found");
+});
+
+app.post ('/upload', function(request, response, next)
+{
+});
+
+app.post('/request', bodyParser.raw(), function(request, response, next)
 {
 	API.call (request.body, function (obj)
 	{
